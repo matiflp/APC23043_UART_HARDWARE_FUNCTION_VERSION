6ba99405ccd82d1c4964bf5a003324252dcf45d8
Matías López <matilpcsd227@gmail.com>
2021-01-24 19:43:28 -0300

    Changed the name and some things about porgramming

--- apc230-43.c
+++ apc230-43.c
@@ -4,68 +4,51 @@
  *  Created on: 4 dic. 2019
  *      Author: Matías López - Jesús López
  */
+//*****************************************************************************
+//
+// apc230-43.c - Driver para manejar el Transceptor RF.
+//
+//*****************************************************************************
 
 #include "apc230-43.h"
 
-uint8_t RXData[7];
-uint8_t TXData = 0;
+int PtrTransmit;
+uint8_t rxData[5];
+uint8_t txData[5];
 uint8_t check = 0;
 uint8_t i = 0;
 
-void APC230_interrupt(void)
+//*****************************************************************************
+void APC230_initPort(void)
 {
-    EUSCI_A_UART_enable(EUSCI_A0_BASE);
+    // Configura los pines para el modo de funcionamiento normal en el RF.
+    APC230_PIN_EN;      // HABILITADO = 1 LÓGICO ; SLEEP = 0 LÓGICO
+    //APC230_PIN_SET;     // MODO SETEO = 0 LÓGICO ; MODO NORMAL = 1 LÓGICo
 
-    EUSCI_A_UART_clearInterrupt(EUSCI_A0_BASE,
-        EUSCI_A_UART_RECEIVE_INTERRUPT);
-
-    // Enable USCI_A0 RX interrupt
-    EUSCI_A_UART_enableInterrupt(EUSCI_A0_BASE,
-        EUSCI_A_UART_RECEIVE_INTERRUPT);
+    // Configure UART pins
+    P1SEL0 |= BIT0 | BIT1;                    // set 2-UART pin as second function
 }
-
-void APC230_configurePins(void)
-{
-    //Configure UART pins
-    GPIO_setAsPeripheralModuleFunctionInputPin(
-        GPIO_PORT_UCA0TXD,
-        GPIO_PIN_UCA0TXD,
-        GPIO_FUNCTION_UCA0TXD
-    );
-    GPIO_setAsPeripheralModuleFunctionInputPin(
-        GPIO_PORT_UCA0RXD,
-        GPIO_PIN_UCA0RXD,
-        GPIO_FUNCTION_UCA0RXD
-    );
-}
-
-bool APC230_configureUART(void)
+//*****************************************************************************
+void APC230_initUART(void)
 {
     // Configure UART
+    UCA0CTLW0 |= UCSWRST;                                               // Put eUSCI in reset
+    UCA0CTLW0 |= UCSSEL__SMCLK;                                         // SMCLK
+    UCA0CTLW0 &= ~(UCMODE_0 + UCSYNC + UCSPB + UC7BIT + UCMSB + UCPEN); // UART mode, Asynchronous mode, One bit-stop, 8-bit data, LSB firt, No parity
     // Baud Rate calculation
-    // 8000000/(16*9600) = 52.083
-    // Fractional portion = 0.083
-    // User's Guide Table 14-4: UCBRSx = 0x49
-    // UCBRFx = int ( (52.083-52)*16) = 1
-    // Ver 14.3.10 Setting a Baud Rate User's Guide
-    EUSCI_A_UART_initParam param = {0};
-    param.selectClockSource = EUSCI_A_UART_CLOCKSOURCE_SMCLK;
-    param.clockPrescalar = 52;
-    param.firstModReg = 1;
-    param.secondModReg = 0x04;
-    param.parity = EUSCI_A_UART_NO_PARITY;
-    param.msborLsbFirst = EUSCI_A_UART_LSB_FIRST;
-    param.numberofStopBits = EUSCI_A_UART_ONE_STOP_BIT;
-    param.uartMode = EUSCI_A_UART_MODE;
-    param.overSampling = EUSCI_A_UART_OVERSAMPLING_BAUDRATE_GENERATION;
-
-    if (STATUS_FAIL == EUSCI_A_UART_init(EUSCI_A0_BASE, &param)) {
-        return 0;
-    }
-
-    return 1;
+    UCA0BR0 = 52;                                                       // N = (8000000/9600) = 833.3333333 => N > 16 => UCA0BR0 = INT(N/16) = 52
+    UCA0MCTLW = 0x4900 | UCOS16 | UCBRF_1;                              // N > 16 => setear OCS16 = 1 ;
+                                                                        // UCBRFx =  INT([(N/16) – INT(N/16)] × 16) = INT((52.083-52)*16) = 1 => setear UCBRF_1
+                                                                        // 8M/9600 - INT(8M/9600) = 0.33 => UCBRSx value = 0x49 (Ver 14.3.10 "Setting a Baud Rate" User's Guide)
 }
-
+//*****************************************************************************
+void APC230_start(void)
+{
+    UCA0CTLW0 &= ~UCSWRST;                                                            // Initialize eUSCI
+    UCA0IFG &= ~(EUSCI_A_UART_RECEIVE_INTERRUPT | EUSCI_A_UART_TRANSMIT_INTERRUPT);   // Limpia interrupciones pendientes
+    UCA0IE |= UCRXIE | UCTXIE;                                                        // Enable USCI_A0 RX interrupt. Solo es necesario esta interrupcion ya que cuando se
+}
+//*****************************************************************************
 #if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
 #pragma vector=USCI_A0_VECTOR
 __interrupt
@@ -78,19 +61,38 @@ void EUSCI_A0_ISR(void)
     {
         case USCI_NONE: break;
         case USCI_UART_UCRXIFG:
-            // Example of recepction
-            RXData[i] = UCA0RXBUF;
+            rxData[i] = UCA0RXBUF;
             i++;
-            if(i == 3)
+            if(i == 1)
+            {
+                __bis_SR_register(LPM0_bits + GIE);
+            }
+            if(rxData[i-1] == 0x0D)
             {
                 i = 0;
-                check = 1;
-                UCA0TXBUF = 0x14;
-                UCA0TXBUF = 0x15;
+                UCA0IFG = 0x00;
+                UCA0STATW = 0x00;
+                __bic_SR_register_on_exit(LPM0_bits + GIE); // Exit LPM0
+                UCA0IE &= ~UCRXIE;
+                UCA0TXBUF = txData[PtrTransmit];
             }
             break;
-       case USCI_UART_UCTXIFG: break;
-       case USCI_UART_UCSTTIFG: break;
-       case USCI_UART_UCTXCPTIFG: break;
+        case USCI_UART_UCTXIFG:
+            i++;
+            PtrTransmit--;                          // Decrement TX byte counter
+            UCA0TXBUF = txData[PtrTransmit];// Load TX buffer
+            if(i == 1)
+            {
+                __bis_SR_register(LPM0_bits + GIE);
+            }
+            if(PtrTransmit < 0)
+            {
+                check = 1;
+                i = 0;
+                __bic_SR_register_on_exit(LPM0_bits + GIE); // Exit LPM0
+            }
+            break;
+        case USCI_UART_UCSTTIFG: break;
+        case USCI_UART_UCTXCPTIFG: break;
     }
 }
--- apc230-43.h
+++ apc230-43.h
@@ -1,23 +1,113 @@
-/*
- * apc230-43.h
- *
- *  Created on: 4 dic. 2019
- *      Author: Matías López - Jesús López
- */
+/**
+  * @file     apc230-43.h
+  * @brief    Driver para manejar el Transceptor RF.
+  * @date     Created on: 4 dic. 2019
+  * @authors  Matías López - Jesús López
+  * @version  1.0
+  */
+//*****************************************************************************
+//
+// apc230-43.h - Driver para manejar el Transceptor RF.
+//
+//*****************************************************************************
 
 #ifndef APC230_43_H_
 #define APC230_43_H_
 
+//*****************************************************************************
+//                              Include
+//*****************************************************************************
 #include "driverlib.h"
-#include "Board.h"
 
-#define APC230_EN   {P1DIR |= BIT6 ; P1OUT |= BIT6;}
-#define APC230_SET  {P5DIR |= BIT0 ; P5OUT |= BIT0;}
+//*****************************************************************************
+//                              Defines
+//*****************************************************************************
+//*****************************************************************************
+//! @name Pines del Transceptor APC230:
+//! \brief Terminales necesarios para comunicarse con el transceptor.
+//! @{
+//*****************************************************************************
+//*****************************************************************************
+//! \details Pin GPIO utlizado para habilitar el módulo de RF (Pin EN).
+//*****************************************************************************
+#define APC230_PIN_EN   {P2DIR |= BIT3 ; P2OUT |= BIT3;}
 
-void AP230C_interrupt(void);
-void APC230_configurePins(void);
-bool APC230_configureUART(void);
+//*****************************************************************************
+//! \detals Pin GPIO que se deja en cero para colocar el RF en modo
+//!         funcionamiento normal (Pin SET).
+//*****************************************************************************
+#define APC230_PIN_SET  {P5DIR |= BIT0 ; P5OUT |= BIT0;}
 
+//*****************************************************************************
+//! @}
+//*****************************************************************************
 
+//*****************************************************************************
+//                              Funciones Prototipos
+//*****************************************************************************
+//*****************************************************************************
+//! \brief Configura los pines que se utilizarán en la comunicación UART.
+//!
+//! \details \b Descripción \n
+//!          Se configura el pin EN del RF para permitir la comunicación con
+//!          el \b MCU, y además el pin SET se coloca en uno para un modo de
+//!          funcionamiento normal ya que de lo contrario sería para una
+//!          configuración de los parametros de la comunicación. Luego se
+//!          configuran los pines UART del MCU, \b P1.0 (TXD) y \b P1.1 (RXD).
+//!          Esto se consigue seleccionando la segunda función de las que
+//!          pueden realizar dichos pines (recordar que los pines \b MCU
+//!          tienen funciones multiplexadas).
+//!
+//! \return \c void.
+//!
+//! \attention Modifica los bits del registro \b P1DIR y del registro
+//!            \b P1SEL.
+//*****************************************************************************
+void APC230_initPort(void);
+
+//*****************************************************************************
+//! \brief Configura la comunicación UART.
+//!
+//! \details \b Descripción \n
+//!          Configura los parámetros comunicación de manera que sean iguales a los
+//!          del transceptor. En primer lugar, se define e inicializa la estructura.
+//!          Luego se elige la fuente de reloj, la cual es \b SMCLK. El Baudrate
+//!          configurado en el transceptor es igual a 9600 bps, la fuente fBRCLK = 8 MHz,
+//!          entonces el factor N = fBRCLK/Baudrate requerido para obtener este Baudrate
+//!          es un valor no entero, por lo tanto, se utiliza un divisor y una etapa
+//!          de modulador para cumplir con que el factor sea lo más proximo posible.
+//!          Al ser N > 16 se utilizará el sobremuestro seteando \b OS16 = 1 en el
+//!          registro \b UCA0MCTLW, luego en el registro \b UCA0BRW se coloca
+//!          \b UCBRx = INT(N/16) y en el registro \b UCA0MCTLW se coloca
+//!          \b UCBRFx = INT([(N/16) - INT(N/16)] × 16) obteniendo con presición el
+//!          Baudrate deseado. Por último, se configuran los parámetros restantes como
+//!          que no hay paridad, cual byte va primero, el bit de stop y el modo UART.
+//!          Se inicia la comunicación pasando  como parametros la direccion base de
+//!          \b EUSCIA y la estructura. \note Para mas información visitar en el documento
+//!          <em>"MSP430FR4xx and MSP430FR2xx family"</em> la sección <em>"22.3.10 Setting a Baud
+//!          Rate"</em>.
+//!
+//! \return \c Un booleano que indica si la comunicación fue exitosa.
+//!
+//! \attention Modifica los bits del registro \b UCA0CTLW0, \b UCA0BR0 y
+//!            \b UCA0MCTLW.
+//*****************************************************************************
+void APC230_initUART(void);
+
+//*****************************************************************************
+//! \brief Inicializa la comunicación UART.
+//!
+//! \details \b Descripción \n
+//!          Una vez finalizada la configuración se resetea el bit \b UCSWRST.
+//!          Luego se resetea el flag en el \b UCA0IFG ya que alguna interrupción
+//!          podría haber quedado pendiente en el \b UCAxRXBUF. Por último, se
+//!          se habiltan las interrupciones de RX.
+//!
+//! \return \c void.
+//!
+//! \attention Modifica los bits del registro \b UCA0CTLW0, \b UCA0IFG y
+//!            \b UCA0IE.
+//*****************************************************************************
+void APC230_start(void);
 
 #endif /* APC230_43_H_ */
